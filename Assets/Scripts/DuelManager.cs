6a81ccfbe5c62e93389c3295af712d7068204f05
toastoffee <931391442@qq.com>
2024-09-10 22:54:21 +0800

    add new states

--- Assets/Scripts/DuelManager.cs
+++ Assets/Scripts/DuelManager.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
-struct Unit
+class Unit
 {
     public int hp;
     public float def, pwr;
@@ -18,34 +19,90 @@ struct Unit
     }
 }
 
+public enum CardType
+{
+    LightPunch,
+    HeavyPunch,
+    Parry,
+    MindPower,
+    Overload,
+}
+
 public class DuelManager : MonoBehaviour
 {
     [SerializeField]
-    private int mPlayerHp = 50, mEnemyHp = 40;
+    private int mPlayerHp = 50, mEnemyHp = 40, mPlayerApPerTurn = 3, mPlayerCardsPerTurn = 4;
 
     [SerializeField]
     private float mPlayerDef = 2.0f, mPlayerPwr = 6.0f, 
                   mEnemyDef = 0.0f, mEnemyPwr = 5.0f;
 
     [SerializeField]
-    private Text mPlayerStatusText, mEnemyStatusText;
-    
+    private Text mPlayerStatusText, mEnemyStatusText, mDuelLogText, mHandCardText;
+
+    [SerializeField]
+    private List<CardType> mCardsLibrary;
+
+    private List<CardType> _mHandCards, _mUsedCards;
+
+    [SerializeField]
+    private int mMaxLogs = 15;
+    private List<string> _mLogs;
+
     private bool _mIsPlayerTurn = true;
+    private int _mPlayerAp, _mPlayerDrawCards;
 
     private Unit _mPlayer, _mEnemy;
 
     private int _mEnemyActIdx = -1;
+
+    private int _mSelectedCardIdx = 0;
     
     private void Start()
     {
         _mPlayer = new Unit(mPlayerHp, mPlayerDef, mPlayerPwr);
         _mEnemy = new Unit(mEnemyHp, mEnemyDef, mEnemyPwr);
+
+        Shuffle(mCardsLibrary);
+        _mHandCards = new List<CardType>();
+        _mUsedCards = new List<CardType>();
+        _mLogs = new();
+
+        _mPlayerAp = mPlayerApPerTurn;
+        _mPlayerDrawCards = mPlayerCardsPerTurn;
         
         UpdateStatusUI();
+        UpdateLogs();
+        
+        DrawCards();
+        UpdateHandCards();
     }
 
     private void Update()
     {
+        // input 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // end turn 
+            EndTurn();
+            UpdateHandCards();
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            // use card
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _mSelectedCardIdx = Mathf.Max(_mSelectedCardIdx - 1, 0);
+            UpdateHandCards();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _mSelectedCardIdx = Mathf.Min(_mSelectedCardIdx + 1, _mHandCards.Count - 1);
+            UpdateHandCards();
+        }
+        
+        
         if (_mIsPlayerTurn)
         {
             // wait for the end button
@@ -55,9 +112,133 @@ public class DuelManager : MonoBehaviour
         {
             _mIsPlayerTurn = true;
             EnemyAct();
+            
+            // end enemy turn, set player turn(recover mana, draw cards)
+            _mPlayerAp = mPlayerApPerTurn;
+            _mPlayerDrawCards = mPlayerCardsPerTurn;
+            DrawCards();
+            UpdateHandCards();
         }
     }
 
+    private void AddLog(string s)
+    {
+        _mLogs.Add(s + '\n');
+        UpdateLogs();
+    }
+    
+    private void UpdateLogs()
+    {
+        string logs = "";
+
+        if (_mLogs.Count <= mMaxLogs)
+        {
+            foreach (var s in _mLogs)
+            {
+                logs += s;
+            }
+        }
+        else
+        {
+            for (int i = _mLogs.Count - mMaxLogs; i < _mLogs.Count; i++)
+            {
+                logs += _mLogs[i];
+            }
+        }
+
+        mDuelLogText.text = logs;
+    }
+    
+    
+    /// <summary>
+    /// cards
+    /// </summary>
+
+    private void Shuffle<T>(List<T> list)
+    {
+        int size = list.Count;
+        while (size > 1)
+        {
+            size--;
+            int randIdx = Random.Range(0, size + 1);
+            (list[randIdx], list[size]) = (list[size], list[randIdx]);
+        }
+    }
+
+    private void DrawCard()
+    {
+        if (mCardsLibrary.Count > 0)    
+        {
+            var card = mCardsLibrary[0];
+            mCardsLibrary.RemoveAt(0);
+            _mHandCards.Add(card);
+        }
+        else    // cards empty
+        {
+            mCardsLibrary = _mUsedCards;
+            Shuffle(mCardsLibrary);
+
+            _mUsedCards = new List<CardType>();
+        }
+    }
+    
+    private void DrawCards()
+    {
+        for (int i = 0; i < mPlayerCardsPerTurn; i++)
+        {
+            DrawCard();
+        }
+    }
+
+    private void DiscardCards()
+    {
+        while (_mHandCards.Count != 0)
+        {
+            var card = _mHandCards[0];
+            _mHandCards.RemoveAt(0);
+            _mUsedCards.Add(card);
+        }
+    }
+
+    private void UpdateHandCards()
+    {
+        string s = "";
+        for (int i = 0; i < _mHandCards.Count; i++)
+        {
+            if (i == _mSelectedCardIdx)
+            {
+                s += "[";
+            }
+
+            switch (_mHandCards[i])
+            {
+                case CardType.LightPunch:
+                    s += "轻击";
+                    break;
+                case CardType.HeavyPunch:
+                    s += "重击";
+                    break;
+                case CardType.Parry:
+                    s += "招架";
+                    break;
+                case CardType.MindPower:
+                    s += "感受念力";
+                    break;
+                case CardType.Overload:
+                    s += "过载";
+                    break;
+            }
+            
+            if (i == _mSelectedCardIdx)
+            {
+                s += "]";
+            }
+
+            s += "\t\t";
+        }
+        mHandCardText.text = s;
+    }
+    
     public void PlayerEndTurn()
     {
         _mIsPlayerTurn = false;
@@ -65,44 +246,57 @@ public class DuelManager : MonoBehaviour
     
     private void EnemyAct()
     {
-        _mEnemyActIdx = _mEnemyActIdx++ % 3;
+        _mEnemyActIdx = (_mEnemyActIdx + 1) % 3;
         if (_mEnemyActIdx == 0)
         {
             // normal attack (1 * pwr)
             float fDmg = 1.0f * _mEnemy.pwr;
-            CauseDamage(fDmg, _mPlayer);
+            var d =CauseDamage(fDmg, _mPlayer);
             EnemyStealDef(1.0f);
+            AddLog($"[敌人]使用攻击对[玩家]造成{d}点伤害");
+            AddLog($"[敌人]从[玩家]处偷取了1点防御");
         }
         else if (_mEnemyActIdx == 1)
         {
             // double attack (2 * 0.25 * pwr)
-            float fDmg = 0.25f * _mEnemy.pwr;
-            CauseDamage(fDmg, _mPlayer);
+            float fDmg = 0.25f * _mEnemy.pwr; 
+            var d = CauseDamage(fDmg, _mPlayer);
             EnemyStealDef(1.0f);
-            CauseDamage(fDmg, _mPlayer);
+            AddLog($"[敌人]使用双重攻击第一击对[玩家]造成{d}点伤害");
+            AddLog($"[敌人]从[玩家]处偷取了1点防御");
+            
+            d = CauseDamage(fDmg, _mPlayer);
             EnemyStealDef(1.0f);
+            AddLog($"[敌人]使用双重攻击第二击对[玩家]造成{d}点伤害");
+            AddLog($"[敌人]从[玩家]处偷取了1点防御");
         }
         else
         {
             // defense improved attack
             float fDmg = 1.0f * _mEnemy.pwr + 1.0f * _mEnemy.def;
-            CauseDamage(fDmg, _mPlayer);
+            var d =CauseDamage(fDmg, _mPlayer);
             EnemyStealDef(1.0f);
+            
+            AddLog($"[敌人]使用防御加成攻击对[玩家]造成{d}点伤害");
+            AddLog($"[敌人]从[玩家]处偷取了1点防御");
         }
         
         UpdateStatusUI();
     }
 
-    private void EndTurn()
+    public void EndTurn()
     {
-        
+        DiscardCards();
+        _mIsPlayerTurn = false;
     }
 
     private void UpdateStatusUI()
     {
         string playerText = $"玩家\n<color=green>生命\t:{_mPlayer.hp}</color>\n" 
                             + $"<color=orange>力量\t:{_mPlayer.pwr}</color>\n" 
-                            + $"<color=brown>防御\t:{_mPlayer.def}</color>";
+                            + $"<color=brown>防御\t:{_mPlayer.def}</color>\n"
+                            + $"<color=yellow>行动点\t:{_mPlayerAp}</color>\n"
+                            + $"<color=silver>下回合抽牌数\t:{_mPlayerDrawCards}</color>";
         string enemyText = $"敌人\n<color=green>生命\t:{_mEnemy.hp}</color>\n" 
                             + $"<color=orange>力量\t:{_mEnemy.pwr}</color>\n" 
                             + $"<color=brown>防御\t:{_mEnemy.def}</color>";
@@ -111,21 +305,30 @@ public class DuelManager : MonoBehaviour
         mEnemyStatusText.text = enemyText;
     }
 
-    private void CauseDamage(float damage, Unit damagedUnit)
+    private int CauseDamage(float damage, Unit damagedUnit)
     {
         // floor the damage to int 
         int iDmg = Mathf.FloorToInt(damage);
         int iDef = Mathf.FloorToInt(damagedUnit.def);
-        
-        // damage equation
-        iDmg = Math.Min(Math.Max(iDmg - iDef, 1), iDmg);
 
+        if (iDmg != 0)
+        {
+            // damage equation
+            iDmg = Math.Clamp(iDmg - iDef, 1, 2 * iDmg);
+        }
+        
         damagedUnit.hp -= iDmg;
+        return iDmg;
     }
 
     private void EnemyStealDef(float amount)
     {
         _mPlayer.def -= amount;
         _mEnemy.def += amount;
+    }
+
+    public void TryPlayCard(CardType type)
+    {
+        
     }
 }
