a83efb69702c64cfdbbfdda132ba9d7fe14b80c2
toastoffee <931391442@qq.com>
2024-09-21 21:24:08 +0800

    add customized cursor

---
+++ Assets/Demos/ToffeeFactory/Scripts/CursorManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToffeeFactory {
+  public class CursorManager : MonoSingleton<CursorManager> {
+    public Texture2D noneCursor;
+    public Texture2D buttonDownCursor;
+    public Texture2D dragCursor;
+    
+    private void Start() {
+      Cursor.SetCursor(noneCursor, Vector2.zero, CursorMode.Auto);  
+    }
+
+    private void FixedUpdate() {
+      if (Input.GetMouseButton(2)) {
+        Cursor.SetCursor(dragCursor, Vector2.zero, CursorMode.Auto);
+      }
+      else if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) {
+        Cursor.SetCursor(buttonDownCursor, Vector2.zero, CursorMode.Auto);
+      } 
+      else {
+        Cursor.SetCursor(noneCursor, Vector2.zero, CursorMode.Auto);  
+      }
+
+
+      
+    }
+  }  
+}
+
