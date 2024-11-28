cf95fad016bc24c27619eedc29cd2fe2023a80b6
toastoffee <931391442@qq.com>
2024-11-28 23:24:50 +0800

    拼尽全力无法战胜

--- Assets/Demos/MarbleSquad/Scripts/ChessPhysics.cs
+++ Assets/Demos/MarbleSquad/Scripts/ChessPhysics.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,8 @@ namespace MarbleSquad {
         private Vector2 _velocity = Vector2.zero;
 
         private bool _isMoving => _velocity.magnitude != .0f;
+
+        public bool isMain = false;
     
         // Start is called before the first frame update
         void Start() {
@@ -41,8 +44,9 @@ namespace MarbleSquad {
             }
 
 
-            if (Input.GetKeyDown(KeyCode.J)) {
-                AddForce(Vector2.right * 3.0f);
+            if (Input.GetKeyDown(KeyCode.J) && isMain) {
+                AddForce(Vector2.right * 5.0f);
+                
             }
         }
 
@@ -59,6 +63,41 @@ namespace MarbleSquad {
             } else {
                 // calculate acceleration F = m * a
                 _velocity += ft / _mass;
+            }
+        }
+
+        public static void HandleChessCollide(ChessPhysics a, ChessPhysics b) {
+            // Vector2 F_dir = (a.transform.position - b.transform.position).normalized;
+            // Vector2 F_v_dir = new Vector2(- F_dir.y, F_dir.x);
+            //
+            // Vector2 v1_h = a._velocity.ProjectLength(F_dir) * F_dir;
+            // Vector2 v2_h = b._velocity.ProjectLength(F_dir) * F_dir;
+            // Vector2 v1_h_new = (2 * b._mass * v2_h + (a._mass - b._mass) * v1_h) / (a._mass + b._mass);
+            // Vector2 v2_h_new = (2 * a._mass * v1_h + (b._mass - a._mass) * v2_h) / (a._mass + b._mass);
+            //
+            // Vector2 v1_v = a._velocity.ProjectLength(F_v_dir) * F_v_dir;
+            // Vector2 v2_v = b._velocity.ProjectLength(F_v_dir) * F_v_dir;
+            // Vector2 v1_v_new = (2 * b._mass * v2_v + (a._mass - b._mass) * v1_v) / (a._mass + b._mass);
+            // Vector2 v2_v_new = (2 * a._mass * v1_v + (b._mass - a._mass) * v2_v) / (a._mass + b._mass);
+            //
+            // Vector2 v1_new = v1_h_new + v1_v_new;
+            // Vector2 v2_new = v2_h_new + v2_v_new;
+            
+            float v1_x_new = (2 * b._mass * b._velocity.x + (a._mass - b._mass) * a._velocity.x) / (a._mass + b._mass);
+            float v2_x_new = (2 * a._mass * a._velocity.x + (b._mass - a._mass) * b._velocity.x) / (a._mass + b._mass);
+            
+            float v1_y_new = (2 * b._mass * b._velocity.y + (a._mass - b._mass) * a._velocity.y) / (a._mass + b._mass);
+            float v2_y_new = (2 * a._mass * a._velocity.y + (b._mass - a._mass) * b._velocity.y) / (a._mass + b._mass);
+
+            a._velocity = new Vector2(v1_x_new, v1_y_new);
+            b._velocity = new Vector2(v2_x_new, v2_y_new);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            Debug.Log("enter");
+            if (other.transform.GetComponent<ChessPhysics>() != null && isMain) {
+                HandleChessCollide(this, other.transform.GetComponent<ChessPhysics>());
+                Debug.Log("?");
             }
         }
     }
