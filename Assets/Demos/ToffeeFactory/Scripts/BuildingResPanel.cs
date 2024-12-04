8c56566ec6d54ee14f740efc350f2567cfdc6716
toastoffee <931391442@qq.com>
2024-12-04 21:33:37 +0800

    打个包包

--- Assets/Demos/ToffeeFactory/Scripts/BuildingResPanel.cs
+++ Assets/Demos/ToffeeFactory/Scripts/BuildingResPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
+// using UnityEditor.SceneManagement;
 
 namespace ToffeeFactory {
   public class BuildingResPanel : MonoBehaviour {
